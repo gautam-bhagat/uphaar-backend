23a2671f7934bcace49eead7fa7e68b4c838af58
gautam-bhagat <gautambhagat.business@gmail.com>
2023-09-17 20:06:37 +0530

    update and delete api complete

--- routes/hospitalAPI.js
+++ routes/hospitalAPI.js
@@ -32,4 +32,74 @@ hospRouter.post('/add',bypass, async (req,res)=>{
     }
 });
 
+hospRouter.put('/add',bypass, async (req,res)=>{
+    let success = 0;
+    try{
+        const {_id, hname,haddress, hcity,hstate,hpincode,hlat,hlong,hphone} = req.body;
+
+        const query = {"_id":_id}
+
+        //check if data exists
+        let found = false
+        await Hospital.exists(query).then(result => {
+            if(result) {found = true;}
+        })
+
+        //if data exists update it
+        if(found){
+            await Hospital.updateOne(query,{'hname':hname,'haddress':haddress, 'hcity':hcity,'hstate':hstate,'hpincode':hpincode,'hlat':hlat,'hlong':hlong,'hphone':hphone});
+
+            success = 1;
+            let message = 'Updated';
+            return res.status(201).json({success,message});
+        }
+
+        //else return not found
+        let message = 'Not Found';
+        return res.status(201).json({success,message});
+       
+
+    }catch(err){
+        console.log(err);
+        success = 0;
+        let message = 'Internal Server Error';
+        res.status(500).json({success,message});
+    }
+});
+
+
+hospRouter.delete('/',bypass,async (req, res) => {
+    try{
+        const {_id } = req.body;
+
+        const query = {"_id":_id}
+
+        //check if data exists
+        let found = false
+        await Hospital.exists(query).then(result => {
+            if(result) {found = true;}
+        })
+
+        //if data exists update it
+        if(found){
+            await Hospital.deleteOne(query);
+
+            success = 1;
+            let message = 'Deleted';
+            return res.status(201).json({success,message});
+        }
+
+        //else return not found
+        let message = 'Not Found';
+        return res.status(201).json({success,message});
+
+    }catch(err){
+        console.log(err);
+        success = 0;
+        let message = 'Internal Server Error';
+        res.status(500).json({success,message});
+    }
+});
+
+
 module.exports = hospRouter;
