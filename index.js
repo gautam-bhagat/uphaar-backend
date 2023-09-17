6acaa23f4a374a3d3208d940537955e9cf237dd3
gautam-bhagat <gautambhagat.business@gmail.com>
2023-09-17 20:21:40 +0530

    homepage

--- index.js
+++ index.js
@@ -11,7 +11,33 @@ connectToMongo();
 
 
 app.get('/', (req, res) => {
-    res.send("Hello World!!")
+    res.status(200).json({
+        'Team' : "Uphaar",
+        'Team Leader' : {
+            'Name' : 'Gautam Bhagat',
+            'Github' : 'https://github.com/gautam-bhagat'
+        },
+        'Team Member 01' : {
+            'Name' : 'Pranav Kale',
+            'Github' : 'https://github.com/pranav-kale-01'
+        },
+        'Team Member 02' :{
+            'Name' : 'Aditya Lad',
+            'Github' : 'https://github.com/Adityalad-25'
+        }, 
+        'Team Member 03' : {
+            'Name' : 'Tarun Rathod',
+            'Github' : 'https://github.com/tarunnnrathoddd'
+        },
+        'Team Member 04' : {
+            'Name' :'Mayuri Narute',
+            'Github' : 'https://github.com/MayuriNarute'
+        },
+        'Team Member 05' : {
+            'Name' : 'Wafiya Mulla',
+            'Github' : 'https://github.com/Wafmulla'
+        },
+    });
 });
 
 app.use('/api/device',require('./routes/deviceAPI'))
