34691a03479268f69d35673af1fb0a56b293560a
gautam-bhagat <gautambhagat.business@gmail.com>
2023-09-17 13:13:40 +0530

    cors enabled

--- index.js
+++ index.js
@@ -1,9 +1,14 @@
 const express = require('express');
+const cors = require('cors');
 const app = express()
 const port = 5000
 
+app.use(cors())
 const connectToMongo = require('./database_connection/connection');
 connectToMongo();
+
+
+
 app.get('/', (req, res) => {
     res.send("Hello World!!")
 });
